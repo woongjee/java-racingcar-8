a8f8fe47e4b1c2843618d982e351618636c4dd08
woongjee <jeewoong7753@naver.com>
2025-10-26 19:56:25 +0900

    feat: 자동차 이름 목록에 대한 유효성 검사
    변경 내용
    - 자동차 이름 목록 내에 빈 이름이 포함되어 있는지 검증하는 'validateCarNamesNotEmpty' 로직 구현

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -6,6 +6,8 @@ public class InputValidator {
     private static final String EMPTY_INPUT_ERROR = "입력값이 비어있습니다.";
     private static final int MAX_CAR_NAME_LENGTH = 5;
     private static final String INVALID_CAR_NAME_LENGTH_ERROR = "자동차 이름은 5자 이하여야 합니다.";
+    private static final String EMPTY_CAR_NAME_ERROR = "자동차 이름이 비어있습니다.";
+
     public static void validateCarNamesInput(String input) {
         if(input == null || input.trim().isEmpty()) {
             throw new IllegalArgumentException(EMPTY_INPUT_ERROR);
@@ -14,11 +16,18 @@ public class InputValidator {
 
     public static void validateCarNames(List<String> carNames) {
         validateCarNameLength(carNames);
+        validateCarNamesNotEmpty(carNames);
     }
 
     private static void validateCarNameLength(List<String> carNames) {
         if(carNames.stream().anyMatch(name -> name.length() > MAX_CAR_NAME_LENGTH)) {
             throw new IllegalArgumentException(INVALID_CAR_NAME_LENGTH_ERROR);
+        }
+    }
+
+    private static void validateCarNamesNotEmpty(List<String> carNames) {
+        if(carNames.stream().anyMatch(String::isEmpty)) {
+            throw new IllegalArgumentException(EMPTY_CAR_NAME_ERROR);
         }
     }
 }
