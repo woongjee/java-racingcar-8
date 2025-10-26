52a6a7a1850aaf0ddefa2bbaa9fd375770d33c11
woongjee <jeewoong7753@naver.com>
2025-10-26 19:39:39 +0900

    feat: 자동차 이름 입력값의 빈 값 검증 로직 구현
    변경 내용
    - InputValidator.validateCarNamesInput 메서드 구현
    - 입력 문자열이
    ull이거나 공백만 포함하는 경우, IllegalArgumentException과 함께 정의된 오류 메시지 반환
    - 예외 메시지를 상수로 관리 (EMPTY_INPUT_ERROR)

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -1,4 +1,11 @@
 package racingcar.validator;
 
 public class InputValidator {
+    private static final String EMPTY_INPUT_ERROR = "입력값이 비어있습니다.";
+
+    public static void validateCarNamesInput(String input) {
+        if(input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException(EMPTY_INPUT_ERROR);
+        }
+    }
 }
