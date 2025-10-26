c8c9037e5966cc8575b20137b3a301ad7d813309
woongjee <jeewoong7753@naver.com>
2025-10-26 20:38:50 +0900

    feat: 자동차 이름을 입력받는 로직 추가
    변경 내용
    - InputView.readCarNames()에서 자동차 이름 입력 받는 로직 추가
    - InputView.parseCarNames()에서 쉼표를 기준으로 자동차 이름 구분 로직 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,27 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import racingcar.validator.InputValidator;
+
 public class InputView {
+    private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String DELIMITER = ",";
+
+    public List<String> readCarNames(){
+        System.out.println(CAR_NAME_INPUT_MESSAGE);
+        String input = Console.readLine();
+        InputValidator.validateCarNamesInput(input);
+
+        List<String> carNames = parseCarNames(input);
+        InputValidator.validateCarNames(carNames);
+        return carNames;
+    }
+
+    public List<String> parseCarNames(String input){
+        return Arrays.stream(input.split(DELIMITER))
+                .map(String::trim)
+                .toList();
+    }
 }
