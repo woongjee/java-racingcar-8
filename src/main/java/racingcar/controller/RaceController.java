2aa3a5b53dbbaeab048168393f620ce1a0043d9c
woongjee <jeewoong7753@naver.com>
2025-10-26 20:54:05 +0900

    feat: Controller의 핵심 기능 구현
    변경 내용
    - 클래스 정의 및 RaceService 객체 의존성 주입
    - 자동차 목록 초기화

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,4 +1,31 @@
 package racingcar.controller;
 
+import java.util.List;
+import racingcar.model.Car;
+import racingcar.model.CarFactory;
+import racingcar.service.RaceService;
+
 public class RaceController {
+    private final RaceService raceService;
+    private List<Car> cars;
+
+    public RaceController(){
+        this.raceService = new RaceService();
+    }
+
+    public void initializeCars(List<String> carNames){
+        this.cars = CarFactory.createCars(carNames);
+    }
+
+    public void race(){
+        raceService.race(cars);
+    }
+
+    public List<Car> getCars(){
+        return cars;
+    }
+
+    public List<String> getWinners(){
+        return raceService.findWinners(cars);
+    }
 }
