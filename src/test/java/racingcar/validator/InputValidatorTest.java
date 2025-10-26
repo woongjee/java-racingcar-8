c5c0aa46ffd6da317a0c345c3ad1a4895414d241
woongjee <jeewoong7753@naver.com>
2025-10-26 19:44:58 +0900

    test: 자동차 이름 길이 초과 예외 테스트 추가
    변경 내용
    - InputValidator.validateCarNames 메서드에 대한 테스트 케이스 추가
    - 자동차 이름이 5자를 초과하는 경우 IllegalArgumentException이 발생하는지 검증

--- src/test/java/racingcar/validator/InputValidatorTest.java
+++ src/test/java/racingcar/validator/InputValidatorTest.java
@@ -2,6 +2,8 @@ package racingcar.validator;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class InputValidatorTest {
@@ -10,5 +12,14 @@ public class InputValidatorTest {
         assertThatThrownBy(() -> InputValidator.validateCarNamesInput(""))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("입력값이 비어있습니다.");
+    }
+
+    @Test
+    void carNameLengthExceedsLimit(){
+        List<String> carNames = Arrays.asList("pobi", "woniii");
+
+        assertThatThrownBy(() -> InputValidator.validateCarNames(carNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 5자 이하여야 합니다.");
     }
 }
