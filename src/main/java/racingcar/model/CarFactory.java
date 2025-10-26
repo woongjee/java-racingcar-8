d1ff483d5c0195dd0490d5237004e91ff1cf865c
woongjee <jeewoong7753@naver.com>
2025-10-26 20:32:38 +0900

    feat: 입력 받은 자동차 이름 리스트 생성 기능 추가
    변경 내용
    - CarFactory.createCars()에 입력받은 자동차 이름들로 생성된 리스트 반환

--- src/main/java/racingcar/model/CarFactory.java
+++ src/main/java/racingcar/model/CarFactory.java
@@ -1,4 +1,14 @@
 package racingcar.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarFactory {
+    public static List<Car> createCars(List<String> carNames){
+        List<Car> cars = new ArrayList<>();
+        for(String name : carNames){
+            cars.add(new Car(name));
+        }
+        return cars;
+    }
 }
