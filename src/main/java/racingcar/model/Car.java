77255ebc901621d40753adfea3b7d2ea848deb44
woongjee <jeewoong7753@naver.com>
2025-10-26 19:12:38 +0900

    feat: Car 모델 구현 및 우승자 판별 로직 추가
    변경 내용
    - 'Car' 클래스: 이름, 위치, 전진 로직('move', 전진 조건 상수 'MOVE_THRESHOLD') 구현
    - 'RaceService' 클래스 : 우승자 목록 반환하는 'findWinners' 로직 구현, 최대 이동 거리를 찾는 내부 메소드('extractWinnerNames') 구현
    
    README에 정의된 핵심 Model 및 Service 로직 구현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,32 @@
 package racingcar.model;
 
 public class Car {
+    private static final int MOVE_THRESHOLD = 4;
+
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move(int randomValue) {
+        if(canMove(randomValue)){
+            position++;
+        }
+    }
+
+    private boolean canMove(int randomValue) {
+        return randomValue >= MOVE_THRESHOLD;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+
+    public String getName() {
+        return name;
+    }
 }
--- src/main/java/racingcar/service/RaceService.java
+++ src/main/java/racingcar/service/RaceService.java
@@ -1,4 +1,28 @@
 package racingcar.service;
 
+import java.util.List;
+import racingcar.model.Car;
+
 public class RaceService {
+    private static final int MIN_RANDOM_VALUE = 0;
+    private static final int MAX_RANDOM_VALUE = 9;
+
+    public List<String> findWinners(List<Car> cars) {
+        int maxPosition = findMaxPosition(cars);
+        return extractWinnerNames(cars, maxPosition);
+    }
+
+    private int findMaxPosition(List<Car> cars) {
+        return cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+    }
+
+    private List<String> extractWinnerNames(List<Car> cars, int maxPosition) {
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .map(Car::getName)
+                .toList();
+    }
 }
