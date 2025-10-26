b228e05b97ca6608a7ae7e807f9c305b665f0f20
woongjee <jeewoong7753@naver.com>
2025-10-26 18:59:31 +0900

    test: 단독 우승자 찾기 기능 테스트 추가
    변경 내용
    - 'RaceService.findWinners' 메소드에 대한 테스트 케이스 추가
    - 하나의 자동차만 가장 높은 위치에 있을 때 해당 자동차의 이름만 반환하는지 확인
    
    단일 우승자를 올바르게 식별하는지 검증

--- src/test/java/racingcar/service/RaceServiceTest.java
+++ src/test/java/racingcar/service/RaceServiceTest.java
@@ -1,4 +1,27 @@
 package racingcar.service;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
+
 public class RaceServiceTest {
+    @Test
+    void findSingleWinner(){
+        RaceService raceService = new RaceService();
+        Car car1 = new Car("pobi");
+        Car car2 = new Car("woni");
+
+        car1.move(4);
+        car1.move(4);
+        car2.move(4);
+
+        List<Car> cars = Arrays.asList(car1, car2);
+        List<String> winners = raceService.findWinners(cars);
+
+        assertThat(winners).containsExactly("pobi");
+    }
 }
