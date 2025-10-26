b090215c5cd291f101da08fb5fe98afab5fd31d4
woongjee <jeewoong7753@naver.com>
2025-10-26 20:40:40 +0900

    feat: 시도 횟수를 입력받는 로직 추가
    변경 내용
    - InputView.readTryCount()에서 시도 횟수를 입력 받는 로직 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,6 +8,7 @@ import racingcar.validator.InputValidator;
 public class InputView {
     private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String DELIMITER = ",";
+    private static final String TRY_COUNT_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
     public List<String> readCarNames(){
         System.out.println(CAR_NAME_INPUT_MESSAGE);
@@ -17,6 +18,13 @@ public class InputView {
         List<String> carNames = parseCarNames(input);
         InputValidator.validateCarNames(carNames);
         return carNames;
+    }
+
+    public int readTryCount(){
+        System.out.println(TRY_COUNT_INPUT_MESSAGE);
+        String input = Console.readLine();
+        InputValidator.validateTryCountInput(input);
+        return Integer.parseInt(input);
     }
 
     public List<String> parseCarNames(String input){
