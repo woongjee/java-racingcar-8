3998f39f3e6c59988bdca9a9b2b96a847fe72dae
woongjee <jeewoong7753@naver.com>
2025-10-26 20:49:59 +0900

    feat: 우승자 출력 로직 추가
    변경 내용
    - OutputView.printWinners()에서 우승자를 출력하는 로직 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -7,6 +7,8 @@ public class OutputView {
     private static final String RESULT_HEADER = "\n실행 결과";
     private static final String NAME_POSITION_SEPARATOR = " : ";
     private static final String POSITION_MARKER = "-";
+    private static final String WINNER_PREFIX = "최종 우승자 : ";
+    private static final String WINNER_DELIMITER = ", ";
 
     public void printReslutHeader(){
         System.out.println(RESULT_HEADER);
@@ -20,5 +22,12 @@ public class OutputView {
 
     private void printCarStatus(Car car){
         System.out.println(car.getName() + NAME_POSITION_SEPARATOR + POSITION_MARKER.repeat(car.getPosition()));
+    }
+
+    public void printWinners(List<String> winners){
+        if(winners.isEmpty()){
+            return;
+        }
+        System.out.println(WINNER_PREFIX + String.join(WINNER_DELIMITER, winners));
     }
 }
