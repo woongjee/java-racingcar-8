9b2199f9d42236010aa14b7073408d26d6c81d47
woongjee <jeewoong7753@naver.com>
2025-10-26 20:47:15 +0900

    feat: 실행 결과 출력 로직 추가
    변경 내용
    - OutputView.printReslutHeader()에서 출력 헤더 출력
    - OutputView.printRaceStatus()과 OutputView.printCarStatus()에서 실행 도중 위치 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,24 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.model.Car;
+
 public class OutputView {
+    private static final String RESULT_HEADER = "\n실행 결과";
+    private static final String NAME_POSITION_SEPARATOR = " : ";
+    private static final String POSITION_MARKER = "-";
+
+    public void printReslutHeader(){
+        System.out.println(RESULT_HEADER);
+    }
+
+    public void printRaceStatus(List<Car> cars){
+        for(Car car : cars){
+            printCarStatus(car);
+        }
+    }
+
+    private void printCarStatus(Car car){
+        System.out.println(car.getName() + NAME_POSITION_SEPARATOR + POSITION_MARKER.repeat(car.getPosition()));
+    }
 }
