0875a68bcbb97bf00d2b6d114c839f35831d2ae2
woongjee <jeewoong7753@naver.com>
2025-10-26 20:25:23 +0900

    test: 자동차 전진 후 위치 확인 테스트
    변경 내용
    - car.move()에 대해 4 이상의 수가 나왔을 경우 위치 검증 테스트 케이스 추가

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,4 +1,16 @@
 package racingcar.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
 public class CarTest {
+    @Test
+    void moveForward(){
+        Car car = new Car("test");
+
+        car.move(4);
+
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
 }
