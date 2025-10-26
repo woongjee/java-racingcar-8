408252623140d05edda1e555829ff89e6ebb060d
woongjee <jeewoong7753@naver.com>
2025-10-26 19:34:27 +0900

    test: 자동차 이름 입력 시 빈 입력값 예외 테스트 추가
    변경 내용
    - 'InputValidator.validateCarNamesInput' 메소드 테스트
    - 빈 문자열("") 입력 시 IllegalArgumentException 발생 및 메시지 검증

--- src/test/java/racingcar/validator/InputValidatorTest.java
+++ src/test/java/racingcar/validator/InputValidatorTest.java
@@ -1,4 +1,14 @@
 package racingcar.validator;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
 public class InputValidatorTest {
+    @Test
+    void emptyInput(){
+        assertThatThrownBy(() -> InputValidator.validateCarNamesInput(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("입력값이 비어있습니다.");
+    }
 }
